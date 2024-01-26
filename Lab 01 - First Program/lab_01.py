161e3db15c5021ec371a119b7b457d73d9730a4d
Michael Bottoms <mjbcowboys9@gmail.com>
2024-01-25 23:26:00 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-hi 
+print("Hello, my name is Michael Bottoms and I once said, \"Is the world loading?\".")
+print("This mind boggling quote was the result of me seeing a speed mirage for the first time.")
+print("""I am not the sharpest tool in the shed,
+unless it is about space.
+In that case, I am pretty sharp.""")
+print("Thank you for coming to my Ted Talk and have a great day!")
