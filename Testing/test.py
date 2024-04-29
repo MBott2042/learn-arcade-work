eac8fc1e1d4c2314392b9dc31f125b4bdc329722
Michael Bottoms <mjbcowboys9@gmail.com>
2024-04-28 23:39:36 -0400

    Work on lab 11

--- Testing/test.py
+++ Testing/test.py
@@ -1,28 +1,110 @@
-import re
+"""
+Array Backed Grid
 
-# This function takes in a line of text and returns
-# a list of words in the line.
+Show how to use a two-dimensional list/array to back the display of a
+grid on-screen.
+"""
+import arcade
+
+# Set how many rows and columns we will have
+ROW_COUNT = 10
+COLUMN_COUNT = 10
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
+
+# This sets the margin between each cell
+# and on the edges of the screen.
+MARGIN = 5
+
+# Do the math to figure out our screen dimensions
+SCREEN_WIDTH = (WIDTH + MARGIN) * ROW_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * COLUMN_COUNT + MARGIN
 
 
-def split_line(line):
-    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
 
+    def __init__(self, width, height):
+        """
+        Set up the application.
+        """
+        super().__init__(width, height)
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        self.grid = []
+        for row in range(ROW_COUNT):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.grid.append([])
+            for column in range(COLUMN_COUNT):
+                self.grid[row].append(0)  # Append a cell
 
-some_names = ['DamianLoveless', 'Rufus Hades', 'Severin the Unpleasant']
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw the grid
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                # Figure out what color to draw the box
+
+                if (self.grid[row][column] == 1):
+                    color = arcade.color.GREEN
+
+                else:
+                    color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        # Change the x/y screen coordinates to grid coordinates
+        column = x // (WIDTH + MARGIN)
+        row = y // (HEIGHT + MARGIN)
+
+        print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
+
+        # Make sure we are on-grid. It is possible to click in the upper right
+        # corner in the margin and go to a grid location that doesn't exist
+        row_range = [row - 1, row, row + 1]
+        column_range = [column - 1, column, column + 1]
+
+        for r in row_range:
+            for c in column_range:
+
+                if r < ROW_COUNT and c < COLUMN_COUNT and r > -1 and c > -1:
+                    if r== row or c == column:
+                    # Flip the location between 1 and 0.
+                        if self.grid[r][c] == 0:
+                            self.grid[r][c] = 1
+                        else:
+                            self.grid[r][c] = 0
+
+            # Flip the location between 1 and 0.
 
 
 def main():
-    names = open("super_villains.txt")
-    for name in names:
-        name = name.strip()
-        for some_name in some_names:
-            if name == some_name:
-                print('hey', name, 'matches')
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
 
-    for some_name in some_names:
-        for name in names:
-            name = name.strip()
-            if name == some_name:
-                print('hey', name, 'matches')
 
+if __name__ == "__main__":
     main()
